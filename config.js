ecdec311b492dd949b8f2c6ee12da1ac6a5e78ff
Desconocido-DRL <114960011+Desconocido-DRL@users.noreply.github.com>
2024-08-17 12:48:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['59172945992', 'FG98', true],
+  ['51965277629', 'Desconocido-DRL, true],
   ['5491168352204']
 ] //Numeros de owner 
 
